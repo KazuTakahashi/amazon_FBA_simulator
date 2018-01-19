e9b2689da20b5c1db8b68f3012025fc53d8af61d
Kazuki Takahashi <dsc666543@gmail.com>
2018-01-19 23:36:35 +0900

    [update]CSS追加・slide_toggle削除、ツールチップ変更

--- js/popup.js
+++ js/popup.js
@@ -8,36 +8,16 @@ $(function(){
         var out_html = $($.parseHTML(data));//parse
 
         $('#popup').empty().append(out_html.filter('#popup')[0].innerHTML);//insert
-
-        // $('#page2').empty().append(out_html.filter('#sub2')[0].innerHTML);//insert
-
-        //var sub_html = out_html.filter(function(index) {
-        //    return $(this).attr("id") === "sub";
-        //})[0].innerHTML;//extract
-        //var sub2_html = out_html.filter(function(index) {
-        //    return $(this).attr("id") === "sub2";
-        //})[0].innerHTML;//extract
-        //$('#page').empty().append(sub_html);//insert
-        //$('#page2').empty().append(sub2_html);//insert
+        // $('.children').hide();
+    
     }).fail(function(jqXHR, textStatus, errorThrown){
         alert('error!!!', textStatus);
-        // エラーページを表示
     });
-    // .then(function(data){
-
-    // },function(jqXHR, textStatus) {
-    //     if(textStatus!=="success") {
-    //         // var txt = "<p>textStatus:"+ textStatus + "</p>" +
-    //         //     "<p>status:"+ jqXHR.status + "</p>" +
-    //         //     "<p>responseText : </p><div>" + jqXHR.responseText +
-    //         //     "</div>";
-    //         // $('#page').html(txt);
-    //         // $('#page2').html(txt);
-    //     }
-    // });
-    // $('#page').load('external.html');
 
 
+    $(document).on("click", ".slide-button", function () {
+        $(this).slideToggle();
+    });
 
 
     var backgroundPage = chrome.extension.getBackgroundPage();
@@ -46,4 +26,5 @@ $(function(){
     //alert(product);
     console.log("poput:", product);
     $('#product-title').children('h1').text(product.title);
+
 });
